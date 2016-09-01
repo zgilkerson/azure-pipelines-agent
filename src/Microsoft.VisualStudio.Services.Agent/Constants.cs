2ed3a946f5ad5fb5a575e87cfa673887b0596cf3
Ting <tihuang@microsoft.com>
2016-09-01 10:45:45 -0400

    send job notification after first request renew. (#568) (#569)

--- src/Microsoft.VisualStudio.Services.Agent/Constants.cs
+++ src/Microsoft.VisualStudio.Services.Agent/Constants.cs
@@ -38,7 +38,7 @@ namespace Microsoft.VisualStudio.Services.Agent
 
         public static class Agent
         {
-            public static readonly string Version = "2.105.4";
+            public static readonly string Version = "2.105.5";
 
 #if OS_LINUX
             public static readonly OSPlatform Platform = OSPlatform.Linux;
