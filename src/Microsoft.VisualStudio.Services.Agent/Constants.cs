7f51dc44f64f29b26d5f0f00c26f99352201418e
Tingluo Huang <tihuang@microsoft.com>
2017-02-09 10:38:52 -0500

    2.112.0 for TFS2017Qu1

--- src/Microsoft.VisualStudio.Services.Agent/Constants.cs
+++ src/Microsoft.VisualStudio.Services.Agent/Constants.cs
@@ -39,7 +39,7 @@ namespace Microsoft.VisualStudio.Services.Agent
 
         public static class Agent
         {
-            public static readonly string Version = "2.111.1";
+            public static readonly string Version = "2.112.0";
 
 #if OS_LINUX
             public static readonly OSPlatform Platform = OSPlatform.Linux;
