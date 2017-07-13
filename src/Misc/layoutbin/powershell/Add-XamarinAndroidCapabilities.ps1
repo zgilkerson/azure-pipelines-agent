51ca0733c7283a8a616ae2d8b8d681f4a353c855
madhurig <madhuri.gummalla@hotmail.com>
2017-07-13 10:35:01 +0530

    switching to check for packages in VSSetup

--- src/Misc/layoutbin/powershell/Add-XamarinAndroidCapabilities.ps1
+++ src/Misc/layoutbin/powershell/Add-XamarinAndroidCapabilities.ps1
@@ -1,30 +1,22 @@
 [CmdletBinding()]
 param()
-$vs15 = Get-VisualStudio_15_0
-if ($vs15 -and $vs15.installationPath) {
-    # End with "\" for consistency with old ShellFolder values.
-    $shellFolder15 = $vs15.installationPath.TrimEnd('\'[0]) + "\"
-    Write-Capability -Name 'VisualStudio_15.0' -Value $shellFolder15
-    $latestVS = $shellFolder15
-    $installDir15 = [System.IO.Path]::Combine($shellFolder15, 'Common7\IDE\ReferenceAssembiles\Microsoft\Framework')
-    [Version]$latestVersion = "0.0.0.0"
-    
-    # check for mono.android.jar under the vs2017 installation folder 
-    try {
-        $androidFiles = Get-ChildItem $installDir15 -Name 'mono.android.jar' -Recurse
-        foreach ($androidFile in $androidFiles) {
-            [Version]$version = $androidFile.TrimStart('MonoAndroid').TrimEnd('mono.android.jar').Trim('\').TrimStart('v')
-            if ($version -gt $latestVersion) {
-                $latestVersion = $version
-            }
+$xamarinAndroidVersion = [Version]'0.0.0.0'
+
+# check VS2017 setup packages for latest Xamarin Android version
+$vs2017Instances = Get-VSSetupInstance -All | Select-VSSetupInstance -Require 'Xamarin.Android.Sdk'
+foreach ($vs2017Instance in vs2017Instances) {
+    $xa = $vs2017Instance.packages | Where { $_.id -match 'Xamarin.Android.Sdk' }
+    if ($xa -and $xa.version) {
+        $version = [Version]$xa.version
+        if ($version -gt $xamarinAndroidVersion) {
+            $xamarinAndroidVersion = $version
         }
-    } catch {
-        Write-Host ($_ | Out-String)
-    }
-    if ($latestVersion -gt [Version]"0.0.0.0") {
-        Write-Capability -Name 'Xamarin.Android' -Value $latestVersion
     }
 }
+if ($latestVersion -gt [Version]'0.0.0.0') {
+        Write-Capability -Name 'Xamarin.Android' -Value $latestVersion
+}
 else {
+    # check legacy registry key for Xamarin Android (VS 2015 and previous versions)
     $null = Add-CapabilityFromRegistry -Name 'Xamarin.Android' -Hive 'LocalMachine' -View 'Registry32' -KeyName 'Software\Novell\Mono for Android' -ValueName 'InstalledVersion'
 }
