81da4f81cbf7ed325c7c59764ae37d7e341209d1
Zak Fields <zafields@microsoft.com>
2017-08-02 20:49:05 -0700

    Update projects to build for Linux ARM w/ .NET 2.0 (#1113)

---
+++ src/Microsoft.VisualStudio.Services.Agent/BuildConstants.cs
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  DO NOT CHECKIN THE GENERATED .cs version of this file
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Microsoft.VisualStudio.Services.Agent
+{
+    public static class BuildConstants
+    {
+        public static class Source
+        {
+            public static readonly string CommitHash = "39925b6b2f91b1811f2543d8f3014a2730eb9829";
+        }
+        
+        public static class AgentPackage
+        {
+            public static readonly string PackageName = "linux-arm";
+        }
+    }
+}
