2131bc8ff9714c6f283b173ac468f64edd2518ae
Tingluo Huang <tihuang@microsoft.com>
2018-08-08 16:36:41 -0400

    patch 2.136.2 for TFS2018U3.

--- src/Microsoft.VisualStudio.Services.Agent/Constants.cs
+++ src/Microsoft.VisualStudio.Services.Agent/Constants.cs
@@ -59,7 +59,7 @@ namespace Microsoft.VisualStudio.Services.Agent
 
         public static class Agent
         {
-            public static readonly string Version = "2.136.1";
+            public static readonly string Version = "2.136.2";
 
 #if OS_LINUX
             public static readonly OSPlatform Platform = OSPlatform.Linux;
